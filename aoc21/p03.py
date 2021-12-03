7f33c4545877f39ffb74dd126cb1e42775630ca2
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-12-04 02:02:00 +0300

    Solved p03b
    Switched to numpy because solving it in Python stdlib was a bit of a pain.

--- aoc21/p03.py
+++ aoc21/p03.py
@@ -1,22 +1,16 @@
+from typing import Callable
+
 from aoc21 import utils
+import numpy as np
 
 
-def gamma_and_epsilon(report: list[str]) -> tuple[int, int]:
-    """Calculates gamma and epsilon for a submarine diagnostic report."""
-    if not report:
-        return 0, 0
-    bits = len(report[0])
-    half = len(report) / 2
-    counters = [0] * bits
-    for line in report:
-        for i, c in enumerate(line):
-            if c == "1":
-                counters[i] += 1
-    for i, counter in enumerate(counters):
-        if counter == half:
-            raise ValueError(f"Same amount of zeros and ones in column {i}: {counter}")
-    g_bits = [c > half for c in counters]
-    e_bits = [not bit for bit in g_bits]
+def gamma_and_epsilon(array: np.ndarray) -> tuple[int, int]:
+    """Calculate gamma and epsilon for a submarine diagnostic report."""
+    if len(array) == 0:
+        raise ValueError("Empty array")
+    diff = np.sum(array == 1, axis=0) - np.sum(array == 0, axis=0)
+    g_bits = list(diff >= 0)
+    e_bits = list(diff < 0)
     return bools_to_int(g_bits), bools_to_int(e_bits)
 
 
@@ -25,7 +19,48 @@ def bools_to_int(xs: list[bool]) -> int:
     return int("".join("1" if x else "0" for x in xs), base=2)
 
 
+def o2_and_co2(array: np.ndarray) -> tuple[int, int]:
+    """Calculate o2 and co2 ratings for a submarine diagnostic report."""
+    o2 = life_support(array, o2_criteria)
+    co2 = life_support(array, co2_criteria)
+    return o2, co2
+
+
+def o2_criteria(column: np.ndarray) -> int:
+    """The criteria for o2: the most common value in the column."""
+    return 1 if np.sum(column == 1) >= np.sum(column == 0) else 0
+
+
+def co2_criteria(column: np.ndarray) -> int:
+    """The criteria for co2: the least common value in the column."""
+    return 0 if np.sum(column == 0) <= np.sum(column == 1) else 1
+
+
+def life_support(array: np.ndarray, bit_criteria: Callable[[np.ndarray], int]) -> int:
+    """Calculate a life support rating for a report based on a custom criteria.
+
+    The criteria determines which bits from the column to keep for subsequent columns.
+    """
+    _, ncols = array.shape
+    for i in range(ncols):
+        column = array[:, i]
+        bit = bit_criteria(column)
+        column_mask = column == bit
+        mask = np.tile(column_mask[:, None], [1, ncols])
+        masked = array[mask]
+        array = masked.reshape(masked.size // ncols, ncols)
+        if array.shape[0] == 1:
+            return bools_to_int(list(array[0] == 1))
+    raise ValueError("No matching row found")
+
+
+def parse_input(lines: list[str]) -> np.ndarray:
+    return np.array([[int(c) for c in line] for line in lines])
+
+
 if __name__ == "__main__":
-    lines = utils.read_input_lines(__file__)
-    g, e = gamma_and_epsilon(lines)
+    array = parse_input(utils.read_input_lines(__file__))
+    g, e = gamma_and_epsilon(array)
     print(g * e)
+    o2, co2 = o2_and_co2(array)
+    print(o2 * co2)
--- tests/test_p03.py
+++ tests/test_p03.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc21.p03 import gamma_and_epsilon
+from aoc21.p03 import gamma_and_epsilon, o2_and_co2, parse_input
 
 
 def b(s):
@@ -8,20 +8,23 @@ def b(s):
 
 
 def test_empty():
-    assert gamma_and_epsilon([]) == (0, 0)
+    with pytest.raises(ValueError):
+        gamma_and_epsilon(parse_input([]))
 
 
 def test_single():
-    assert gamma_and_epsilon(["01"]) == (b("01"), b("10"))
+    assert gamma_and_epsilon(parse_input(["01"])) == (b("01"), b("10"))
 
 
 def test_2_equal():
     assert (
         gamma_and_epsilon(
-            [
-                "01",
-                "01",
-            ]
+            parse_input(
+                [
+                    "01",
+                    "01",
+                ]
+            )
         )
         == (b("01"), b("10"))
     )
@@ -30,45 +33,86 @@ def test_2_equal():
 def test_3_with_majority():
     assert (
         gamma_and_epsilon(
-            [
-                "01",
-                "01",
-                "10",
-            ]
+            parse_input(
+                [
+                    "01",
+                    "01",
+                    "10",
+                ]
+            )
         )
         == (b("01"), b("10"))
     )
 
 
 def test_4_with_1_equal():
-    with pytest.raises(ValueError):
-        assert gamma_and_epsilon(
-            [
-                "01",
-                "01",
-                "10",
-                "00",
-            ]
+    assert (
+        gamma_and_epsilon(
+            parse_input(
+                [
+                    "01",
+                    "01",
+                    "10",
+                    "00",
+                ]
+            )
         )
+        == (b("01"), b("10"))
+    )
 
 
 def test_example():
     assert (
         gamma_and_epsilon(
-            [
-                "00100",
-                "11110",
-                "10110",
-                "10111",
-                "10101",
-                "01111",
-                "00111",
-                "11100",
-                "10000",
-                "11001",
-                "00010",
-                "01010",
-            ]
+            parse_input(
+                [
+                    "00100",
+                    "11110",
+                    "10110",
+                    "10111",
+                    "10101",
+                    "01111",
+                    "00111",
+                    "11100",
+                    "10000",
+                    "11001",
+                    "00010",
+                    "01010",
+                ]
+            )
         )
         == (b("10110"), b("01001"))
+    )
+
+
+def test_o2_empty():
+    with pytest.raises(ValueError):
+        o2_and_co2(parse_input([]))
+
+
+def test_o2_no_lines_with_0_for_co2():
+    with pytest.raises(ValueError):
+        o2_and_co2(parse_input(["1010"]))
+
+
+def test_o2_2_values():
+    assert o2_and_co2(parse_input(["01", "11"])) == (b("11"), b("01"))
+
+
+def test_o2_3_values():
+    assert o2_and_co2(parse_input(["01", "10", "11"])) == (b("11"), b("01"))
+
+
+def test_first_3_from_example():
+    assert (
+        o2_and_co2(
+            parse_input(
+                [
+                    "00100",
+                    "11110",
+                    "10110",
+                ]
+            )
+        )
+        == (b("11110"), b("00100"))
     )
