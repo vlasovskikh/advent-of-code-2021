55ed9ea0c445126db169e76c5868e7d527a52e58
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-12-04 23:51:19 +0300

    Solved p04b

--- aoc21/p04.py
+++ aoc21/p04.py
@@ -1,8 +1,11 @@
+from typing import Iterator
+
 import numpy as np
+
 from aoc21 import utils
 
 
-def play_bingo(boards: np.ndarray, numbers: list[int]) -> tuple[int, int]:
+def play_bingo(boards: np.ndarray, numbers: list[int]) -> Iterator[tuple[int, int]]:
     """Play bingo on a set of `boards` using `numbers` and return the sum of
     unmarked numbers on the winning board and the last called number.
 
@@ -12,32 +15,40 @@ def play_bingo(boards: np.ndarray, numbers: list[int]) -> tuple[int, int]:
     assert n_rows == n_cols
     win_combination = np.full(n_rows, True)
     bingo = np.full(boards.shape, False)
+    playing = np.full(boards.shape, True)
     for number in numbers:
         bingo[boards == number] = True
         for axis in [1, 2]:  # Columns and rows
-            win_arrays = np.all(bingo == win_combination, axis=axis)
+            win_arrays = np.all((bingo & playing) == win_combination, axis=axis)
             if np.any(win_arrays):
-                return sum_unmarked_for_winner(boards, bingo, win_arrays), number
-    raise ValueError("No winning board")
+                index = win_index(win_arrays)
+                yield sum_unmarked(boards, bingo, index), number
+                playing[index] = False
+                if not np.any(playing):
+                    return
+    raise ValueError("Unfinished boards, but not numbers left")
 
 
-def sum_unmarked_for_winner(
-    boards: np.ndarray, bingo: np.ndarray, win_arrays: np.ndarray
-) -> int:
-    """Sum the unmarked number on the winning board, given the winning arrays.
+def sum_unmarked(boards: np.ndarray, bingo: np.ndarray, index: int) -> int:
+    """Sum the unmarked number on the board by its index.
 
     `bingo` is a 3D-array of called check marks `True`. It has the same shape as
     `boards`.
+    """
+    win_board = boards[index]
+    win_bingo = bingo[index]
+    unmarked = ~win_bingo
+    return int(np.sum(win_board[unmarked]))
+
+
+def win_index(win_arrays: np.ndarray) -> int:
+    """Index of the winning board from the winning arrays.
 
     `win_arrays` is a 2D-array of (board, axis) with `True` when we have a winning
     combination for this axis on this board.
     """
     win_boards = np.any(win_arrays, axis=1)
-    win_index = np.where(win_boards)[0][0]
-    win_board = boards[win_index]
-    win_bingo = bingo[win_index]
-    unmarked = ~win_bingo
-    return int(np.sum(win_board[unmarked]))
+    return np.where(win_boards)[0][0]
 
 
 def parse_input(lines: list[str]) -> tuple[np.ndarray, list[int]]:
@@ -57,8 +68,11 @@ def parse_input(lines: list[str]) -> tuple[np.ndarray, list[int]]:
 
 def main():
     boards, numbers = parse_input(utils.read_input_lines(__file__))
-    sum_unmarked, number = play_bingo(boards, numbers)
-    print(sum_unmarked * number)
+    winners = list(play_bingo(boards, numbers))
+    first_sum, first_n = winners[0]
+    print(first_sum * first_n)
+    last_sum, last_n = winners[-1]
+    print(last_sum * last_n)
 
 
 if __name__ == "__main__":
