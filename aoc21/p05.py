de7b75e9250beace83e2a41f0ac46695636d082c
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-12-05 15:26:16 +0300

    Solved p05b

--- aoc21/p05.py
+++ aoc21/p05.py
@@ -1,6 +1,5 @@
 import itertools
 import re
-from typing import Iterator
 
 import numpy as np
 
@@ -9,25 +8,42 @@ from aoc21 import utils
 Line = tuple[int, int, int, int]
 
 
-def dangerous_areas(lines: list[Line]) -> int:
+def dangerous_areas(lines: list[Line], *, diagonals: bool) -> int:
+    """Find all the dangerous areas areas where at least two lines interect.
+
+    Set `diagonals` to `True` to count diagonals too, otherwise it's only horizontal
+    and vertical lines.
+    """
     field = np.full(dimentions(lines), 0)
-    for x1, y1, x2, y2 in horizontal_and_vertical(lines):
-        x1, x2 = sorted([x1, x2])
-        y1, y2 = sorted([y1, y2])
-        field[y1 : y2 + 1, x1 : x2 + 1] += 1  # noqa: E203
+    for x1, y1, x2, y2 in lines:
+        if x1 == x2 or y1 == y2:
+            field[slice(*slice_tuple(y1, y2)), slice(*slice_tuple(x1, x2))] += 1
+        elif diagonals:
+            x_slice = slice_tuple(x1, x2, for_range=True)
+            y_slice = slice_tuple(y1, y2, for_range=True)
+            for x, y in zip(range(*x_slice), range(*y_slice)):
+                field[y, x] += 1
     return len(field[field >= 2])
 
 
+def slice_tuple(x1: int, x2: int, *, for_range: bool = False) -> tuple[int, int, int]:
+    """Return (start, stop, step) for the specified range of `x1`, `x2`.
+
+    `x1` and `x2` can be specified in any order.
+
+    `range()` uses `stop = -1` to stop at the first element, while `slice()` uses
+    `None` for this purpose. You can specify the desired behaviour using `for_range`.
+    """
+    sign = np.sign(x2 - x1) if x1 != x2 else 1
+    stop = x2 + sign
+    return x1, stop if stop >= 0 or for_range else None, sign
+
+
 def dimentions(lines: list[Line]) -> tuple[int, int]:
+    """The dimentions of a field sufficient to accomodate these `lines`."""
     x = max(itertools.chain((line[0] for line in lines), (line[2] for line in lines)))
     y = max(itertools.chain((line[1] for line in lines), (line[3] for line in lines)))
     return x + 1, y + 1
-
-
-def horizontal_and_vertical(lines: list[Line]) -> Iterator[Line]:
-    for x1, y1, x2, y2 in lines:
-        if x1 == x2 or y1 == y2:
-            yield x1, y1, x2, y2
 
 
 def parse_input(lines: list[str]) -> list[Line]:
@@ -41,7 +57,8 @@ def parse_input(lines: list[str]) -> list[Line]:
 
 def main():
     lines = parse_input(utils.read_input_lines(__file__))
-    print(dangerous_areas(lines))
+    print(dangerous_areas(lines, diagonals=False))
+    print(dangerous_areas(lines, diagonals=True))
 
 
 if __name__ == "__main__":
--- tests/test_p05.py
+++ tests/test_p05.py
@@ -2,7 +2,7 @@ from aoc21.p05 import parse_input, dangerous_areas
 
 
 def test_example():
-    data = """\
+    data = """
 0,9 -> 5,9
 8,0 -> 0,8
 9,4 -> 3,4
@@ -12,5 +12,49 @@ def test_example():
 0,9 -> 2,9
 3,4 -> 1,4
 0,0 -> 8,8
-5,5 -> 8,2""".splitlines()
-    assert dangerous_areas(parse_input(data)) == 5
+5,5 -> 8,2
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=False) == 5
+    assert dangerous_areas(parse_input(data), diagonals=True) == 12
+
+
+def test_diagonal_cross():
+    data = """
+0,0 -> 2,2
+0,2 -> 2,0
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=True) == 1
+
+
+def test_normal_and_diagonal_crosses():
+    data = """
+0,0 -> 2,2
+0,2 -> 2,0
+0,1 -> 2,1
+1,0 -> 1,2
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=True) == 1
+
+
+def test_diagonal_cross_backwards():
+    data = """
+2,2 -> 0,0
+2,0 -> 0,2
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=True) == 1
+
+
+def test_normal_cross():
+    data = """
+1,0 -> 1,2
+0,1 -> 2,1
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=True) == 1
+
+
+def test_normal_cross_backwards():
+    data = """
+1,2 -> 1,0
+2,1 -> 0,1
+""".strip().splitlines()
+    assert dangerous_areas(parse_input(data), diagonals=True) == 1
