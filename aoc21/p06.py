4150a84f686c6540b3dfbd59d974f7cde68c2fb7
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-12-06 21:57:07 +0300

    Solved p06b

--- aoc21/p06.py
+++ aoc21/p06.py
@@ -1,27 +1,44 @@
-import numpy as np
+import itertools
 
 from aoc21 import utils
 
 
-def model_fish(fish: np.ndarray, days: int) -> np.ndarray:
+def model_fish(fish: list[int], days: int) -> int:
     """Model the life of a school of fish for the specified amount of days."""
-    fish = fish.copy()
+    counts = compress_fish(fish)
     for _ in range(days):
-        fish -= 1
-        new = np.full(np.sum(fish < 0), 8)
-        fish = np.concatenate((fish, new))
-        fish[fish < 0] = 6
-    return fish
+        # i:  0 1 2 3 4 5 6 7 8 9
+        # s: -1 0 1 2 3 4 5 6 7 8
+        counts.pop(state(-1))
+        new = counts[state(-1)]
+        counts.append(new)
+        counts[state(6)] += new
+    return sum(counts[1:])
 
 
-def parse_input(lines: list[str]) -> np.ndarray:
-    return np.array([int(s) for line in lines for s in line.split(",")])
+def state(s: int) -> int:
+    """Get the index of compressed fish from the living state of fish."""
+    return s + 1
+
+
+def compress_fish(fish: list[int]) -> list[int]:
+    """Returns an array of fish count in each of the fish states: [-1, 0..8].
+
+    * -1 — the state of creating a new fish
+    * 0..8 — the amount of days before creating a new fish
+    """
+    grouped = {k: len(list(g)) for k, g in itertools.groupby(sorted(fish))}
+    return [grouped.get(i, 0) for i in range(-1, 9)]
+
+
+def parse_input(lines: list[str]) -> list[int]:
+    return [int(s) for line in lines for s in line.split(",")]
 
 
 def main():
     fish = parse_input(utils.read_input_lines(__file__))
-    print(len(model_fish(fish, 80)))
-    print(len(model_fish(fish, 256)))
+    print(model_fish(fish, 80))
+    print(model_fish(fish, 256))
 
 
 if __name__ == "__main__":
