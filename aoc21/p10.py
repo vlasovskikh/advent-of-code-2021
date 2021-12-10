7a7e8262a8f16630d4f3529de0dc8608307943e9
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-12-10 22:35:00 +0300

    Solved p10b

--- aoc21/p10.py
+++ aoc21/p10.py
@@ -1,12 +1,19 @@
 from collections import defaultdict
 
-from funcparserlib.parser import a, many, forward_decl, NoParseError, finished
-
+from funcparserlib.parser import (
+    a,
+    many,
+    forward_decl,
+    NoParseError,
+    finished,
+    Parser,
+    maybe,
+)
 
 from aoc21 import utils
 
 
-def check_syntax(lines: list[str]) -> dict[str, int]:
+def valid_chunks_parser() -> Parser:
     chunk = forward_decl()
     chunks = many(chunk)
     parens = -a("(") + chunks + -a(")")
@@ -14,19 +21,59 @@ def check_syntax(lines: list[str]) -> dict[str, int]:
     curlies = -a("{") + chunks + -a("}")
     angles = -a("<") + chunks + -a(">")
     chunk.define(parens | squares | curlies | angles)
-    tree = chunks + -finished
+    return chunks + -finished
 
+
+def incomplete_chunks_parser() -> Parser:
+    chunk = forward_decl()
+    chunks = many(chunk)
+    parens = a("(") + chunks + maybe(a(")"))
+    squares = a("[") + chunks + maybe(a("]"))
+    curlies = a("{") + chunks + maybe(a("}"))
+    angles = a("<") + chunks + maybe(a(">"))
+    chunk.define(parens | squares | curlies | angles)
+    return chunks + -finished
+
+
+def corrupted_char(line: str) -> str | None:
+    try:
+        valid_chunks_parser().parse(line)
+    except NoParseError as e:
+        error_pos = e.state.max
+        if error_pos < len(line):
+            return line[error_pos]
+    return None
+
+
+def check_syntax(lines: list[str]) -> dict[str, int]:
     errors: dict[str, int] = defaultdict(lambda: 0)
-
     for line in lines:
-        try:
-            tree.parse(line)
-        except NoParseError as e:
-            error_pos = e.state.max
-            if error_pos < len(line):
-                errors[line[error_pos]] += 1
-
+        if c := corrupted_char(line):
+            errors[c] += 1
     return errors
+
+
+def autocomplete_lines(lines: list[str]) -> list[str]:
+    return [autocomplete(line) for line in lines if not corrupted_char(line)]
+
+
+def autocomplete(line: str) -> str:
+    chunks = incomplete_chunks_parser().parse(line)
+    return closing_brackets(chunks)
+
+
+def closing_brackets(chunks: list) -> str:
+    closing = {
+        "(": ")",
+        "[": "]",
+        "{": "}",
+        "<": ">",
+    }
+    if not chunks:
+        return ""
+    start, children, stop = chunks[-1]
+    c = "" if stop else closing[start]
+    return closing_brackets(children) + c
 
 
 def syntax_error_score(errors: dict[str, int]) -> int:
@@ -39,9 +86,29 @@ def syntax_error_score(errors: dict[str, int]) -> int:
     return sum(scoring[k] * v for k, v in errors.items())
 
 
+def autocomplete_score(completion: str) -> int:
+    scoring = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    }
+    s = 0
+    for c in completion:
+        s = s * 5 + scoring[c]
+    return s
+
+
+def autocomplete_lines_score(completions: list[str]) -> int:
+    winners = list(sorted(autocomplete_score(c) for c in completions))
+    assert len(winners) % 2 == 1
+    return winners[len(winners) // 2]
+
+
 def main():
     lines = utils.read_input_lines(__file__)
     print(syntax_error_score(check_syntax(lines)))
+    print(autocomplete_lines_score(autocomplete_lines(lines)))
 
 
 if __name__ == "__main__":
--- tests/test_p10.py
+++ tests/test_p10.py
@@ -1,4 +1,10 @@
-from aoc21.p10 import check_syntax, syntax_error_score
+from aoc21.p10 import (
+    check_syntax,
+    syntax_error_score,
+    autocomplete,
+    autocomplete_lines_score,
+    autocomplete_lines,
+)
 
 
 def test_example():
@@ -16,3 +22,8 @@ def test_example():
 """.strip().splitlines()
     lines = [line.strip() for line in lines]
     assert syntax_error_score(check_syntax(lines)) == 26397
+    assert autocomplete_lines_score(autocomplete_lines(lines)) == 288957
+
+
+def test_autocomplete_1():
+    assert autocomplete("[({(<(())[]>[[{[]{<()<>>") == "}}]])})]"
